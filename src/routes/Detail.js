60d03191f67ceea93dc14bc19f1690db9cc4aab4
hwahyeon <hwahyeon@knou.ac.kr>
2023-11-29 03:21:06 +0900

    Update Detail.js

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,20 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+
+function Detail({ toDos }) {
+  const myId = useParams();
+  const toDo = toDos.find((toDo) => toDo.id === myId.id);
+
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+    </>
+  );
+}
+
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Detail);
