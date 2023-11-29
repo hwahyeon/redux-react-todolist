10a489927a865d24bbaa232f8a41fc13278a138b
hwahyeon <hwahyeon@knou.ac.kr>
2023-11-30 00:36:38 +0900

    Refactored with createSlice

--- src/store.js
+++ src/store.js
@@ -1,37 +1,17 @@
-import { createStore } from "redux";
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+import {  configureStore,  createSlice} from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
 
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
-
-/* const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: uuidv4() }, ...state];
-    case deleteToDo.type:
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: uuidv4() });
+    },
+    remove: (state, action) => state.filter(toDo => toDo.id !== action.payload)
   }
-};*/
-
-// createReducer can modify mutate state
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: uuidv4() });
-    // state.push doesn't return anything. just mutate the state
-  },
-  [deleteToDo]: (state, action) =>
-    state.filter((toDo) => toDo.id !== action.payload),
-  // new state should be returned
 });
 
-const store = configureStore({ reducer });
+export const { add, remove } = toDos.actions;
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
-
-export default store;
+export default configureStore({ reducer: toDos.reducer });
