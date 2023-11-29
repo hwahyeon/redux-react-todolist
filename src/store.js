b080263114d4e5451c581f8898f724ddb753fd86
hwahyeon <hwahyeon@knou.ac.kr>
2023-11-30 00:00:30 +0900

    Changed codes with createAction

--- src/store.js
+++ src/store.js
@@ -1,29 +1,16 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id,
-  };
-};
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: uuidv4() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      return [{ text: action.payload, id: uuidv4() }, ...state];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
@@ -33,7 +20,7 @@ const store = createStore(reducer);
 
 export const actionCreators = {
   addToDo,
-  deleteToDo
+  deleteToDo,
 };
 
 export default store;
