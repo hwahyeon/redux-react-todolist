419ac9b0616e672af3c8f99ee03810b79e264b74
hwahyeon <hwahyeon@knou.ac.kr>
2023-11-30 00:14:38 +0900

    Changed codes with createReducer

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
+/* const reducer = (state = [], action) => {
   switch (action.type) {
     case addToDo.type:
       return [{ text: action.payload, id: uuidv4() }, ...state];
@@ -14,7 +14,18 @@ const reducer = (state = [], action) => {
     default:
       return state;
   }
-};
+};*/
+
+// createReducer can modify mutate state
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: uuidv4() })
+    // state.push doesn't return anything. just mutate the state
+  },
+  [deleteToDo]: (state, action) => 
+    state.filter((toDo) => toDo.id !== action.payload)
+    // new state should be returned
+})
 
 const store = createStore(reducer);
 
