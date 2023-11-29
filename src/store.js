70ce3f2e1e1fef4a4a6682baf52c62ed44cadcec
hwahyeon <hwahyeon@knou.ac.kr>
2023-11-30 01:45:41 +0900

    Added functions for save and load from Local Storage

--- src/store.js
+++ src/store.js
@@ -1,15 +1,40 @@
-import {  configureStore,  createSlice} from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
+
+const loadFromLocalStorage = () => {
+  try {
+    const toDoState = localStorage.getItem("toDos");
+    if (toDoState === null) return [];
+    return JSON.parse(toDoState);
+  } catch (e) {
+    return [];
+  }
+};
+
+const saveToLocalStorage = (state) => {
+  try {
+    const toDoState = JSON.stringify(state);
+    localStorage.setItem("toDos", toDoState);
+  } catch (e) {
+    console.log("error");
+  }
+};
 
 const toDos = createSlice({
   name: "toDosReducer",
-  initialState: [],
+  initialState: loadFromLocalStorage(),
   reducers: {
     add: (state, action) => {
-      state.push({ text: action.payload, id: uuidv4() });
+      const newState = [...state, { text: action.payload, id: uuidv4() }];
+      saveToLocalStorage(newState);
+      return newState;
     },
-    remove: (state, action) => state.filter(toDo => toDo.id !== action.payload)
-  }
+    remove: (state, action) => {
+      const newState = state.filter((toDo) => toDo.id !== action.payload);
+      saveToLocalStorage(newState);
+      return newState;
+    },
+  },
 });
 
 export const { add, remove } = toDos.actions;
